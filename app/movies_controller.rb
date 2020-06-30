8c3bd697c4f4593e480ed8aef5e74b4e6fff54ef
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 18:21:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -34,8 +34,7 @@ def can_be_created_in_a_block(args = __)
 end
 
 def can_get_the_first_item_in_the_database
-  sql = "SELECT * FROM movies WHERE id = 1"
-  DB[:conn].execute(sql)
+  movie = Movie.first
 end
 
 def can_get_the_last_item_in_the_database

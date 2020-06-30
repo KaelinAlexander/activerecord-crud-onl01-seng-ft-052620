03e90d98f6b261f17661a2cf9fd4bd1ee2045f7d
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 18:12:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new
+  movie = Movie.
   movie.title = "This is a title."
   movie.save
 end
@@ -20,7 +20,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.new(attributes)
+  movie = Movie.create_with(attributes)
 end
 
 def can_be_created_in_a_block(args = __)

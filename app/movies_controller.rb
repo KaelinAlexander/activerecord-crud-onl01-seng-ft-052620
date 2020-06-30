4182f4e4e9dd5023a383cf8f762e220440516c33
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-30 18:22:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,12 +38,11 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie = Movie.last
+  movie = Movie.last
 end
 
 def can_get_size_of_the_database
-  sql = "SELECT COUNT(id) FROM movies"
-  DB[:conn].execute(sql)
+  
 end
 
 def can_find_the_first_item_from_the_database_using_id
